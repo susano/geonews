909696a883af7ca917262e0159d056fd58ab30bc
andy boot <bootandy@gmail.com>
2012-02-18 18:46:07 +0000

    maps on home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,20 @@ require 'twitter'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  	def initialize
+  	def after_initialize
   		Twitter.configure do |config|
-  			config.consumer_key = 'EGqdOAkoapvXWxtOpAf3Bg'
-			config.consumer_secret = 'IoaNO6sPKOqKPE5uj7AFcVQE9L40RjrKXIaQBDEOqM'
-  			config.oauth_token = '160344744-RBMzDurTvkepdZNYECjWUvsJuNB9Y2E4RHqwpqe5'
-  			config.oauth_token_secret = 'iGCOcwGTh6uiOjLIW2pKKMgEGR1KUQKRp4U9szMryFU'
+  			config.consumer_key = 'AC34dB3nkzpLrJy3T4w'
+			config.consumer_secret = 'h1zbWL0jJxksPqrAPUuft0H8RvD1SrYkxX3pLcqv7c'
+  			config.oauth_token = '60975184-z121haiBlffAnXF30BeB2LYWZ9Uhn22rNR8DE5vvs'
+  			config.oauth_token_secret = 'oxoCZSe4hbpSyTiy4WrhYhyVRCIgnoqtOXmmznnwNaA'
 		end
   	end
 
 	def list
 		@tweets = Twitter.user_timeline("@Launch48")
+
 	end
+
 	
 	def places
 		@places = Twitter.places_nearby(:ip => "144.82.250.184")
