a07f246536137abbca3dd323a9e347e900be5186
Jean Krohn <jb.krohn@gmail.com>
2012-02-18 13:15:00 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geonews::Application.config.secret_token = '2540287c30d7fee4c4c293a52e104832829cd8bf16488072b88ef6f7382d37247411c61c6352afbfe64af3e9c63fb92883d028608cc3994e05de2c024d75c5e4'
