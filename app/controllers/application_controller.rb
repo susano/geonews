dead95395ad74a24779405ac0d7ae9f50d169b74
Jean Krohn <jb.krohn@gmail.com>
2012-02-18 18:41:56 +0000

    initialize => after_initialize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,15 @@ require 'twitter'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  	def initialize
-  		Twitter.configure do |config|
-  			config.consumer_key = 'EGqdOAkoapvXWxtOpAf3Bg'
-			config.consumer_secret = 'IoaNO6sPKOqKPE5uj7AFcVQE9L40RjrKXIaQBDEOqM'
-  			config.oauth_token = '160344744-RBMzDurTvkepdZNYECjWUvsJuNB9Y2E4RHqwpqe5'
-  			config.oauth_token_secret = 'iGCOcwGTh6uiOjLIW2pKKMgEGR1KUQKRp4U9szMryFU'
+
+	def after_initialize
+		Twitter.configure do |config|
+			config.consumer_key       = 'EGqdOAkoapvXWxtOpAf3Bg'
+			config.consumer_secret    = 'IoaNO6sPKOqKPE5uj7AFcVQE9L40RjrKXIaQBDEOqM'
+			config.oauth_token        = '160344744-RBMzDurTvkepdZNYECjWUvsJuNB9Y2E4RHqwpqe5'
+			config.oauth_token_secret = 'iGCOcwGTh6uiOjLIW2pKKMgEGR1KUQKRp4U9szMryFU'
 		end
-  	end
+	end
 
 	def list
 		@tweets = Twitter.user_timeline("@Launch48")
